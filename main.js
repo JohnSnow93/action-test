1108dd39238b30141d340f39f0dc97c15defe691
jon <wb-zx840695@antgroup.com>
2020-10-21 13:44:29 +0800

    基本完成

--- main.js
+++ main.js
@@ -2,7 +2,7 @@ const lagou = require("./lagou");
 const processRawDataForEcharts = require("./processRawDataForEcharts");
 
 async function start() {
-  // await lagou();
+  await lagou();
   await processRawDataForEcharts();
 }
 
